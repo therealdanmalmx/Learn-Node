108e64c9f84587787677957b0f8fc4448b4a3724
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-06-03 21:35:50 +0200

    Add LatLng autocomplete

--- starter-files/public/javascripts/modules/autoComplete.js
+++ starter-files/public/javascripts/modules/autoComplete.js
@@ -1,10 +1,14 @@
 const autocomplete = (input, latInput, lngInput) => {
-    if(!input) {
-        return;   
-    }
-    const dropdown = new google.maps.places.Autocomplete(input)
-    console.log(input,latInput,lngInput)
+  if (!input) {
+    return;
+  }
+  const dropdown = new google.maps.places.Autocomplete(input);
 
+  dropdown.addListener("place_changed", () => {
+    const place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
 };
 
 export default autocomplete;
