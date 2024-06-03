a8cd70074e93077df1f557faa4db5b28ea38b889
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-06-03 21:38:13 +0200

    Prevent enter from submitting

--- starter-files/public/javascripts/modules/autoComplete.js
+++ starter-files/public/javascripts/modules/autoComplete.js
@@ -9,6 +9,11 @@ const autocomplete = (input, latInput, lngInput) => {
     latInput.value = place.geometry.location.lat();
     lngInput.value = place.geometry.location.lng();
   });
+  input.on("keydown", (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  });
 };
 
 export default autocomplete;
